75f6e35a6e670ee2c8197ecaf4813d5d481280ce
nikit <nikitka.lyashko@bk.ru>
2022-11-26 17:14:27 +0300

    Good job

--- main.py
+++ main.py
@@ -29,53 +29,21 @@ def balls():
 
 
 
-@wrap.always(delay=10)
-def fly():
-    global speed_x
-    if true_1==False:
-        for clov_in_lict in a_lot_of_ball:
-
-
-            sprite.move(clov_in_lict["id"],clov_in_lict["speed_x"],clov_in_lict["speed_y"])
-
-            right_ball=sprite.get_right(clov_in_lict["id"])
-            if right_ball>=500:
-                clov_in_lict["speed_x"]=-clov_in_lict["speed_x"]
-
-            left_ball=sprite.get_left(clov_in_lict["id"])
-            if left_ball<=0:
-                clov_in_lict["speed_x"]=-clov_in_lict["speed_x"]
-
-            top_ball=sprite.get_top(clov_in_lict["id"])
-            if top_ball<0:
-                clov_in_lict["speed_y"]=-clov_in_lict["speed_y"]
-
-            bottom_ball=wrap.sprite.get_bottom(clov_in_lict["id"])
-            if bottom_ball>500:
-                clov_in_lict["speed_y"]=-clov_in_lict["speed_y"]
-
 @wrap.on_key_down(wrap.K_1)
 def stop():
-    global true_1
-    true_1=not true_1
+    global mode
+    mode="stop"
     
-    # if true_1 == False:
-    #     true_1 = True
-    # elif true_1 == True:
-    #     true_1 = False
-
+@wrap.on_key_down(wrap.K_3)
+def next_fly():
+    global mode
+    mode="fly"
 
 @wrap.on_key_down(wrap.K_2)
 def drops():
-    global true_2
-    true_2=not true_2
+    global mode
+    mode="fall"
 
-
-@wrap.always(delay=10)
-def dropping():
-    if true_2==True:
-        for abc in a_lot_of_ball:
-            wrap.sprite.move(abc["id"],0,2)
 
 
 
@@ -141,29 +109,52 @@ list_drow = []
 costume =["blue","red","green"]
 m=0
 y=0
-true_1=False
-true_2=False
 
-hello=drow("E: скрыть показать детали:" )
+mode="fly"
+
+drow("E: скрыть показать детали:" )
 balls_on_face=drow("на экране столько шариков:" + str(len(a_lot_of_ball)))
 balls_on_face_1=drow("столько создается в секунду"+str(m))
-balls_on_face_2=drow("Del: удалить элементы с экрана" )
-balls_on_face_3=drow("1 - это остановка или запус шариков")
+drow("Del: удалить элементы с экрана" )
+drow("1 - это остановка")
+drow("2- это падение")
+drow("3- это свободный полет")
 
 
 
-# hello = wrap.sprite.add_text("E:скрыть показать детали", 25, 15, text_color=[110, 255, 32])
-# balls_on_face = wrap.sprite.add_text("на экране столько шариков:" + str(len(a_lot_of_ball)), 50, 30, text_color=[110, 255, 32])
-# balls_on_face_1 = wrap.sprite.add_text("столько создается в секунду:" + str(m), 50, 45, text_color=[110, 255, 32])
-# balls_on_face_2 = wrap.sprite.add_text("Del: удалить элементы с экрана", 50, 60, text_color=[110, 255, 32])
-# balls_on_face_3 = wrap.sprite.add_text("1 - это остановка или запус шариков", 50, 75, text_color=[110, 255, 32])
+@wrap.always(delay=10)
+def fly():
+    global speed_x
+    if mode=="fly":
+        for clov_in_lict in a_lot_of_ball:
 
-#
-# wrap.sprite.move_left_to(hello,0)
-# wrap.sprite.move_left_to(balls_on_face, 0)
-# wrap.sprite.move_left_to(balls_on_face_1, 0)
-# wrap.sprite.move_left_to(balls_on_face_2, 0)
-# wrap.sprite.move_left_to(balls_on_face_3, 0)
+
+            sprite.move(clov_in_lict["id"],clov_in_lict["speed_x"],clov_in_lict["speed_y"])
+
+            right_ball=sprite.get_right(clov_in_lict["id"])
+            if right_ball>=500:
+                clov_in_lict["speed_x"]=-clov_in_lict["speed_x"]
+
+            left_ball=sprite.get_left(clov_in_lict["id"])
+            if left_ball<=0:
+                clov_in_lict["speed_x"]=-clov_in_lict["speed_x"]
+
+            top_ball=sprite.get_top(clov_in_lict["id"])
+            if top_ball<0:
+                clov_in_lict["speed_y"]=-clov_in_lict["speed_y"]
+
+            bottom_ball=wrap.sprite.get_bottom(clov_in_lict["id"])
+            if bottom_ball>500:
+                clov_in_lict["speed_y"]=-clov_in_lict["speed_y"]
+
+v=1
+@wrap.always(delay=10)
+def dropping():
+    global v
+    if mode=="fall":
+        for abc in a_lot_of_ball:
+            wrap.sprite.move(abc["id"],0,v)
+            v+=0.1
 
 
 
